a837fb7a1e8551dbccc26726fcd6d48277bd87af
RickDev <ricllima@hotmail.com>
2021-04-02 12:54:39 -0300

    first commit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,31 @@
+import {signIn, signOut, useSession} from 'next-auth/client'
+
 import {FaGithub} from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ?  (
+  console.log(session)
+
+  return session ?  (
     <button 
     className={styles.signInButton}
     type="button"
-    
+    onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
-     Rick Lopes
+       {session.user.name}
      <FiX  color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     (
       <button 
-      className={styles.signInButton}
       type="button"
-     
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
       >
         <FaGithub  color="#eba417"/>
         SignIn GitHub
